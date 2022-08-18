800b94f0e6d8b94c156fd114832c5699bfa864c9
falahfkh <falahfakhruddin@hotmail.com>
2022-08-18 19:55:56 +0700

    set static secret key

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config(object):
     basedir    = os.path.abspath(os.path.dirname(__file__))
 
     # Set up the App SECRET_KEY
-    SECRET_KEY = os.urandom(32)
-    #SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
+    #SECRET_KEY = os.urandom(32)
+    SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
 
     # This will create a file in <app> FOLDER
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
